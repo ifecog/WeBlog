bd116d2e7dfb4e2d86d61ad5ac23a30ed3f7b00a
progIfe <ifeoluwasamson90@gmail.com>
2022-12-01 10:36:31 +0100

    requirements update

--- weblog/settings.py
+++ weblog/settings.py
@@ -138,8 +138,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'weblog/static'), ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
