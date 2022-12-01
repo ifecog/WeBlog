b8042684a1f370ef9093872ec907ebc73a5d4524
progIfe <ifeoluwasamson90@gmail.com>
2022-12-01 11:49:04 +0100

    media files

--- weblog/settings.py
+++ weblog/settings.py
@@ -141,7 +141,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'weblog/static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # messages
